dfa2660c2c12db8b1ed69b199ae45378697abff3
Serhat Aras <srharas@gmail.com>
2018-10-30 21:59:54 +0300

    Crawled data of 20181030.json

--- crawler/automator.bat
+++ crawler/automator.bat
@@ -1,12 +1,16 @@
 @ECHO OFF
 title New York Times Crossword Crawler!	
-chdir /d ****************************************
+chdir /d ************************************
 SET dateext = _%date:~-4,4%%date:~-7,2%%date:~-10,2%
-scrapy crawl scrapper -o test.json 
-ren  "test.json" "%date:~-4,4%%date:~-7,2%%date:~-10,2%.json"
+IF EXIST "%date:~-4,4%%date:~-7,2%%date:~-10,2%.json" (
+   	echo "Already crawled the data from NY Times for %date:~-10,2%-%date:~-7,2%-%date:~-4,4%, no need to scrape again!"
+) ELSE (
+	scrapy crawl scrapper -o test.json 
+   	 ren  "test.json" "%date:~-4,4%%date:~-7,2%%date:~-10,2%.json"
+)
 echo Crawling Completed!
 git config --global user.name "Serhat Aras"
-git config --global user.email "***************************"
+git config --global user.email "*************"
 git config --global color.ui true
 git add --all
 git commit -m "Crawled data of %date:~-4,4%%date:~-7,2%%date:~-10,2%.json"
