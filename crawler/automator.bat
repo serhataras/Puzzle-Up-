69306a466b0e6d741b85d863fc550ee59621c7e6
Serhat Aras <srharas@gmail.com>
2018-10-30 04:14:12 +0300

    Crawled data of 20181030.json

--- crawler/automator.bat
+++ crawler/automator.bat
@@ -1,12 +1,12 @@
 @ECHO OFF
 title New York Times Crossword Crawler!	
-chdir /d D:\GitHub Projects\PuzzleUp\crawler\crawler\data
+chdir /d ****************************************
 SET dateext = _%date:~-4,4%%date:~-7,2%%date:~-10,2%
 scrapy crawl scrapper -o test.json 
 ren  "test.json" "%date:~-4,4%%date:~-7,2%%date:~-10,2%.json"
 echo Crawling Completed!
 git config --global user.name "Serhat Aras"
-git config --global user.email "srharas@gmail.com"
+git config --global user.email "***************************"
 git config --global color.ui true
 git add --all
 git commit -m "Crawled data of %date:~-4,4%%date:~-7,2%%date:~-10,2%.json"
