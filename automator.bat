1bb8345b8df0deb75383b01ee21907744a88f066
Serhat Aras <serhat.aras@ug.bilkent.edu.tr>
2018-12-13 23:17:43 +0300

    Auto git

--- automator.bat
+++ automator.bat
@@ -2,7 +2,7 @@
 
 title New York Times Crossword Crawler!	
 
-chdir /d D:\Projects\Puzzle-Up-\crawler\
+chdir /d D:\Github\Puzzle-Up\crawler\
 echo "%date:~-7,2%-%date:~-10,2%-%date:~-4,4%.json" 
 chdir ..
 .\crawler\Scripts\activate  
