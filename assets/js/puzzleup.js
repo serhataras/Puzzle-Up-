7cb8aa8d31c5ee23771d594bbf813c0905e11f90
Serhat Aras <serhat.aras@ug.bilkent.edu.tr>
2018-12-13 23:14:31 +0300

    Front End Updates Vol 2
    Mısc. Buttons Added. Binding with javascript completed. Need play method.

--- assets/js/puzzleup.js
+++ assets/js/puzzleup.js
@@ -29,12 +29,12 @@ var puzzlenumbers = [0,0,0,0,0,
     var date;
     var fullpath; 
 
-    var accross_clues_numbers;
-    var accross_clues;
-    var down_clues_numbers;
-    var down_clues;
-    var grid_data;
-    var grid;
+    var accross_clues_numbers = [];
+    var accross_clues= [];
+    var down_clues_numbers= [];
+    var down_clues= [];
+    var grid_data= [];
+    var grid= [];
 
     var Author;
     var publishDate;
@@ -61,20 +61,44 @@ var puzzlenumbers = [0,0,0,0,0,
         loadJSON(function(json) {
             console.log(json); // this will log out the json object
             crossword_JSON=json;
-
+/*
             grid_data=crossword_JSON[0].Grid[0].gridType;
-           
             accross_clues_numbers=crossword_JSON[0].left[1].number;
             accross_clues=crossword_JSON[0].left[1].clue;
-            
             down_clues_numbers=crossword_JSON[0].right[1].number;
             down_clues=crossword_JSON[0].right[1].clue;
-
+*/
+            grid_data = crossword_JSON.grid;
+            clues =  crossword_JSON.clues;
+            date = crossword_JSON.date;
+            title = crossword_JSON.title;
+            clueNumbers = crossword_JSON.clueNumbers;
             console.log(grid_data);
+            console.log(clues);
+            console.log(date);
+            console.log(title);
+            console.log(clueNumbers);
+            
+            for(var i = 0 ; i < 10; i++)
+            {
+                if(i<5){
+                    accross_clues_numbers[i] = clueNumbers[i];
+                    accross_clues[i] = clues[i];
+                }
+                else{
+                    i = i -5;
+                    down_clues_numbers[i] = clueNumbers[i+5];
+                    down_clues[i] =  clues[i+5]; 
+                    i = i +5;
+                }
+            }
             console.log(accross_clues_numbers);
+        
             console.log(accross_clues);
+            
             console.log(down_clues_numbers);
             console.log(down_clues);
+
             parseJSON();
             initializeCrosswordInstance();
             initClues(accross_clues_numbers,accross_clues,down_clues_numbers, down_clues);
@@ -103,7 +127,9 @@ var puzzlenumbers = [0,0,0,0,0,
     
 }
 
-
+function reset(){
+    setPathandLoadJSON();
+}
 
 //########################################
 /*
@@ -114,21 +140,27 @@ function parseJSON(){
 
         for(var i = 0 ; i < 25; i++)
         {
-            if(grid_data[i].includes("Cell-block--1oNaD")){
-                lockedboxids[i]=0;
+            if(grid_data[i][0] == -1 )
+                lockedboxids[i] = 0;
+            else if( grid_data[i][0] == null)
+                lockedboxids[i] = 1;
+            else 
+            {
+                lockedboxids[i] = 1;
+                puzzlenumbers[i] = grid_data[i][0];
             }
-            else{
-                lockedboxids[i]=1;
-                if(grid_data[i].includes("font-size=\"33.33\">")){
-                    //charcount = 20
-                    var index= grid_data[i].indexOf("font-size=\"33.33\">")+18
-                    puzzlenumbers[i]=grid_data[i][index];
-                }
-                else puzzlenumbers[i]=0;
-            }    
+            if( -1 == grid_data[i][1])
+                solution_puzzle[i] = "";
+            else
+            solution_puzzle[i] = grid_data[i][1];
+                
         }
+       
         console.log(puzzlenumbers);
         console.log(lockedboxids);
+        console.log(solution_puzzle);
+        
+
 }
 
  // function 
@@ -144,7 +176,8 @@ function parseJSON(){
                 node.id = "box"+i;    
                 myWrapper.appendChild(node);
                 var textbox = document.createElement("input");
-                textbox.className="GridInput"
+                textbox.className="GridInput";
+                textbox.id="GridInput"+i;
                 textbox.type = "text";
                 textbox.setAttribute("maxlength","1");
                 
@@ -172,6 +205,31 @@ function parseJSON(){
             }
         }
     }
+    function removeElement(elementId) {
+        // Removes an element from the document
+        var element = document.getElementById(elementId);
+        element.parentNode.removeChild(element);
+    }
+
+    function reveal(){
+    
+        //myWrapper.innerHTML="";
+        for(var i = 0 ; i < 25; i++)
+        {
+            if(solution_puzzle[i])
+            {
+                removeElement("GridInput"+i)
+                var myWrapper= document.getElementById("box"+i);
+                var textnode = document.createElement("h1");
+                textnode.className = "revealed";
+                textnode.innerHTML = ""+solution_puzzle[i];
+             
+                myWrapper.appendChild(textnode);
+            }
+        }
+    }
+
+
 
     function initClues(accross_clues_numbers,accross_clues,down_clues_numbers, down_clues)
     {
